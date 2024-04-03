f8ffc928f25c2067bd4361715156815b7d4a597c
William <wosborn1@vols.utk.edu>
2024-04-03 13:21:04 -0400

    updates

--- scheduler.cpp
+++ scheduler.cpp
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <chrono>
 #include <string>
-//#include <boost>
+#include <thread>
+#include <mutex>
 
 #include "algos.cpp"
 
@@ -11,6 +12,19 @@ using namespace std;
 
 long total_workload = 0; //keeps track of total work done
 
+void print_elapsed(){
+    chrono::steady_clock::time_point now = chrono::high_resolution_clock:now();
+
+}
+void interrupter(){
+
+    while (true)
+    {
+        this_thread::sleep_for(chrono::milliseconds(5000));
+        cout << "good morning" << endl;
+    }
+
+}
 void produceJobs(int num_jobs, vector<Job*>& jobs){
     //generate random jobs and add them to the list of incoming jobs
     for (int i = 0; i < num_jobs; ++i){
@@ -46,7 +60,9 @@ int main(int argc, char* argv[]){
     produceJobs(num_jobs, jobs);
     cout << "Produced " << jobs.size() << " jobs" << endl;
     schedule_jobs(num_jobs, jobs, algo);
+    thread t(interrupter);
     runJobs(num_jobs, jobs);
+    t.join();
     auto end = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(end-begin);
     cout << "Jobs ran in " << duration.count() << " microseconds." << endl;
